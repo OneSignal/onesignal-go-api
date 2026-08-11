4542e160d952b048e361ca69326281ffe3f0006e
OneSignal <noreply@onesignal.com>
2026-08-11 23:50:10 +0000

    fix: add v5.12.2 package updates

--- model_create_api_key_response.go
+++ model_create_api_key_response.go
@@ -3,7 +3,7 @@ OneSignal
 
 A powerful way to send personalized messages at scale and build effective customer engagement strategies. Learn more at onesignal.com
 
-API version: 5.11.1
+API version: 5.11.2
 Contact: devrel@onesignal.com
 */
 
